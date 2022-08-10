51b18234108cf37c18eebc5c0bf4683015d18574
Mykola Hohsadze <Mykola_Hohsadze@epam.com>
2022-08-11 00:56:30 +0300

    add print of matrix

--- include/matrix_simd_aarch64.h
+++ include/matrix_simd_aarch64.h
@@ -5,6 +5,6 @@
 #define MATRIX_COLUMNS_OFFSET   4
 #define MATRIX_LENGTH_OFFSET    8
 #define MATRIX_ARRAY_OFFSET     12
-#define MATRIX_SIZE             18
+#define MATRIX_SIZE             20
 
 #endif // MATRIX_SIMD_AARCH64_H_
